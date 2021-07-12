9995c00cdd8ce3eca090bba6a35de93f97224279
kjnb-0 <80792196+kjnb-0@users.noreply.github.com>
2021-07-12 15:04:45 -0700

    update with functional questions/answers

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
-// to do....
-//write function to generate questions and answers using key/value pairs
-//write function to keep score
-
-
 //questions and answers objects
 var quizQuestions = [
   {
-    questiontitle: "placeholder1",
-    questionchoices: "placeholderchoices",
-    questionanswer: "placeholderanswers",
+    question: "bla bla placeholder",
+    choices: ["A", "B", "C", "D"],
+    answer: "C",
+  },
+  {
+    question: "example",
+    choices: ["1", "2", "3", "4"],
+    answer: "4",
+  },
+  {
+    question: "bla bla placeholder",
+    choices: ["A", "B", "C", "D"],
+    answer: "C",
+  },
+  {
+    question: "bla bla placeholder",
+    choices: ["A", "B", "C", "D"],
+    answer: "C",
+  },
+  {
+    question: "bla bla placeholder",
+    choices: ["A", "B", "C", "D"],
+    answer: "C",
   },
 ];
 
+//quiz flow variables
+var lastQuestion = false;
+var currentQuestionIndex = 0;
+
+//general variables
 var startButton = document.querySelector("#start-btn");
 var questions = document.querySelector("#questions");
-var choices = document.querySelector("#choices");
-
-var lastQuestion = false;
+var choices1 = document.querySelector("#choices1");
+var choices2 = document.querySelector("#choices2");
+var choices3 = document.querySelector("#choices3");
+var choices4 = document.querySelector("#choices4");
 
 //timer variables
 var timer;
@@ -28,8 +49,6 @@ var score;
 var scoreCount = 0;
 var scoreElement = document.querySelector("#score-count");
 
-
-
 // write welcome message in the start quiz function
 function startQuiz() {
   var welcomeMessage =
@@ -37,45 +56,70 @@ function startQuiz() {
   console.log(welcomeMessage);
   //alert(welcome);
   scoreElement.textContent = scoreCount;
+  timerElement.textContent = timerCount;
+  document.querySelector("#testarea").classList.remove("hidden");
   startTimer();
   generateQuestions();
 }
 
 function rightAnswer() {
-    var rightAnswerMessage = "Correct!";
-    //alert(rightAnswerMessage);
-    console.log(rightAnswerMessage)
-    scoreCount += 10;
-    scoreElement.textContent = scoreCount
+  var rightAnswerMessage = "Correct";
+  //alert(rightAnswerMessage);
+  scoreCount += 10;
+  scoreElement.textContent = scoreCount;
+
+  currentQuestionIndex++;
+  if (currentQuestionIndex === quizQuestions.length) {
+    endQuiz();
+  } else {
+    generateQuestions();
+  }
 }
 
-function wrongAnswer(){
-    var wrongAnswerMessage = "Incorrect";
-    //alert(wrongAnswerMessage);
-    console.log(wrongAnswerMessage)
-    timerCount -= 10;
-    scoreElement.textContent = scoreCount;
+function wrongAnswer() {
+  var wrongAnswerMessage = "Incorrect";
+  //alert(wrongAnswerMessage);
+  timerCount -= 10;
+  scoreElement.textContent = scoreCount;
+  currentQuestionIndex++;
+  if (currentQuestionIndex === quizQuestions.length) {
+    endQuiz();
+  } else {
+    generateQuestions();
+  }
 }
 
-//THIS NEEDS WORK! access questions, answers
-//display question and answer
-//pick correct, display right/wrong function
-function generateQuestions(){
-    console.log(quizQuestions);
-    //console.log(Object.values(quizQuestions,quizchoices))
-    
+function generateQuestions() {
+  //tutor helped with notation for accessing objects
+  questions.textContent = quizQuestions[currentQuestionIndex].question;
+  choices1.textContent = quizQuestions[currentQuestionIndex].choices[0];
+  choices2.textContent = quizQuestions[currentQuestionIndex].choices[1];
+  choices3.textContent = quizQuestions[currentQuestionIndex].choices[2];
+  choices4.textContent = quizQuestions[currentQuestionIndex].choices[3];
 }
-
-
+//user picks answer and moves to next question
+function chooseAnswer(event) {
+  //listen to user click and evaluate against value
+  if (event.target.textContent === quizQuestions[currentQuestionIndex].answer) {
+    rightAnswer();
+  } else {
+    wrongAnswer();
+  }
+}
 
 function endQuiz() {
   var endQuizMessage = "End of quiz! Submit your scores";
-  console.log(endQuizMessage);
   //alert(endQuizMessage)
-
+  lastQuestion = true;
+  //hides test area when quiz ends
+  document.querySelector("#testarea").classList.add("hidden");
   //function to submit initials and score
   //play again
 }
+
+//function to enter name/scores, store in local storage
+
+//function for high scores button to access local storage and display info
 
 //timer function to start and stop quiz timer
 //if timer is 0 or the last question is reached, ends game
@@ -92,5 +136,7 @@ function startTimer() {
     }
   }, 1000);
 }
+
+document.querySelector("#choicesbox").addEventListener("click", chooseAnswer);
 
 startButton.addEventListener("click", startQuiz);
